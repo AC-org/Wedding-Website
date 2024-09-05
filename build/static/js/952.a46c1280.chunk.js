481c9418d6dd94007cf005571eb62bd141b9af95
AC-AIV <amanda@ai-vision.se>
2024-09-05 17:10:25 +0200

    start over

--- build/static/js/952.a46c1280.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwedding_website=self.webpackChunkwedding_website||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.a46c1280.chunk.js.map
